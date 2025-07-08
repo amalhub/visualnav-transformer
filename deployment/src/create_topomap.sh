b64d502f7687d816672cbfef203864bfeb273fdb
Amal <amal.gunatilake@gmail.com>
2025-07-08 11:42:35 +0000

    Update configs to work with Go2 robot

--- deployment/src/create_topomap.sh
+++ deployment/src/create_topomap.sh
@@ -10,20 +10,21 @@ tmux splitw -v -p 50 # split it into two halves
 tmux selectp -t 0    # go back to the first pane
 tmux splitw -h -p 50 # split it into two halves
 
-# Run roscore in the first pane
+# Run ros2 daemon in the first pane
 tmux select-pane -t 0
-tmux send-keys "roscore" Enter
+tmux send-keys "ros2 topic list" Enter
 
-# Run the create_topoplan.py script with command line args in the second pane
+# Run the create_topomap.py script with command line args in the second pane
 tmux select-pane -t 1
+tmux send-keys "conda_base" Enter
 tmux send-keys "conda activate gnm_deployment" Enter
 tmux send-keys "python create_topomap.py --dt 1 --dir $1" Enter
 
-# Change the directory to ../topomaps/bags and run the rosbag play command in the third pane
+# Change the directory to ../topomaps/bags and run the ros2 bag play command in the third pane
 tmux select-pane -t 2
 tmux send-keys "mkdir -p ../topomaps/bags" Enter
 tmux send-keys "cd ../topomaps/bags" Enter
-tmux send-keys "rosbag play -r 1.5 $2" # feel free to change the playback rate to change the edge length in the graph
+tmux send-keys "ros2 bag play $2 --rate 1.5" # feel free to change the playback rate to change the edge length in the graph
 
 # Attach to the tmux session
 tmux -2 attach-session -t $session_name
