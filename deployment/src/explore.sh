ef2e4c86bdcafd24418f2c4693c952236f540769
Amal <amal.gunatilake@uts.edu.au>
2025-07-14 12:37:41 +0800

    Update run scripts

--- deployment/src/explore.sh
+++ deployment/src/explore.sh
@@ -16,20 +16,21 @@ tmux selectp -t 0    # go back to the first pane
 
 # Run the roslaunch command in the first pane
 tmux select-pane -t 0
-tmux send-keys "roslaunch vint_locobot.launch" Enter
+tmux send-keys "go2run" Enter
 
 # Run the navigate.py script with command line args in the second pane
 tmux select-pane -t 1
 # tmux send-keys "conda activate vint_deployment" Enter
-tmux send-keys "python explore.py $@" Enter
+tmux send-keys "go2cam" Enter
 
 # Run the teleop.py script in the third pane
 tmux select-pane -t 2
 # tmux send-keys "conda activate vint_deployment" Enter
-tmux send-keys "python joy_teleop.py" Enter
+tmux send-keys "python explore.py $@" Enter
 
 # Run the pd_controller.py script in the fourth pane
 tmux select-pane -t 3
+tmux send-keys "conda_base" Enter
 tmux send-keys "conda activate vint_deployment" Enter
 tmux send-keys "python pd_controller.py" Enter
 
